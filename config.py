b5e5c1c3d67e4108e48fbd3ea69bda624338610c
Marco <irotaicnoc@gmail.com>
2026-02-22 01:56:22 +0100

    updated parameters

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 assets_path = 'assets/'
-excel_icon_name_light = 'excel_icon_light_2.png'
-excel_icon_name_dark = 'excel_icon_light_2.png'
+excel_icon_name_light = 'excel_icon_light_1.png'
+excel_icon_name_dark = 'excel_icon_light_1.png'
 
 ui_theme = 'System'  # Options: 'System', 'Light', 'Dark'
 # ui_theme = 'Light'
