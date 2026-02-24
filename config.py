c002681e3fc4925fb03b574dbea466d27e17b033
Marco <irotaicnoc@gmail.com>
2026-02-24 04:31:19 +0100

    refactoring

--- config.py
+++ config.py
@@ -2,9 +2,11 @@ assets_path = 'assets/'
 excel_icon_name_light = 'excel_icon_light_1.png'
 excel_icon_name_dark = 'excel_icon_light_1.png'
 
-ui_theme = 'System'  # Options: 'System', 'Light', 'Dark'
-# ui_theme = 'Light'
-color_theme = 'blue'  # Options: blue, dark-blue, green, dark-green, purple, dark-purple, orange, dark-orange
+# Options: 'System', 'Light', 'Dark'
+# ui_theme = 'System'
+ui_theme = 'Light'
+# Options: blue, dark-blue, green, dark-green, purple, dark-purple, orange, dark-orange
+color_theme = 'blue'
 
 window_title = 'Quotation Tool'
 window_geometry = '800x800'
