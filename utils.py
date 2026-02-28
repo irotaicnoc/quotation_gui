9c43692e39d3d40d6ec3f603bed73e2ad37d7f85
Marco <marco.conciatori@unipr.it>
2026-02-28 02:43:04 +0100

    changed path resolution for pyinstaller

--- utils.py
+++ utils.py
@@ -1,7 +1,22 @@
-def load_stylesheet(file_name="/style.qss"):
+import os
+import sys
+
+import config
+
+
+def load_stylesheet(file_name="style.qss"):
+
+    # Check if running as a PyInstaller bundle
+    if getattr(sys, 'frozen', False):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+
+    # Build the correct absolute path
+    file_path = os.path.join(base_path, config.style_folder_path, file_name)
 
     try:
-        with open(f"styles/{file_name}", "r") as f:
+        with open(file_path, "r") as f:
             return f.read()
     except FileNotFoundError:
         return ""
